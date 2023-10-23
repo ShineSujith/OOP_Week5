eceb970f393c092c6f8b19777d8db6644cd475b2
Shine Sujith <G00414454@atu.ie>
2023-10-23 16:58:12 +0100

    added getUserInput method

--- src/main/java/ie/atu/Main.java
+++ src/main/java/ie/atu/Main.java
@@ -1,7 +1,9 @@
 package ie.atu;
-
 public class Main {
     public static void main(String[] args) {
         Person student1 = new Person("Shine", "Sujith", 19);
+        Person student2 = new Person();
+        student1.displayInfo();
+        student2.getUserInput();
     }
 }
